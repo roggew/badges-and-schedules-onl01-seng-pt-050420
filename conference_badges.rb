42254f041235ccfb864c5ca31bfbb813fb8e4028
William Rogge <williamroggeiv@gmail.com>
2020-05-11 03:35:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,6 +7,6 @@ speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(speakers)
   message = []
-  speakers.each {|name| message << "Hello, my name is #{name}"}
+  speakers.each {|name| message << "Hello, my name is #{name}."}
   message 
 end
