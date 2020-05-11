f13e054c8f0c5dc3481182425ef1a8e177e7e750
William Rogge <williamroggeiv@gmail.com>
2020-05-11 04:56:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,12 @@ def assign_rooms(speakers)
   speakers.each_with_index.map {|name, index| "Hello, #{name}! You'll be assigned to room #{index+1}!"}
 end
 
-def printer
-  puts batch_badge_creator
-  puts assign_rooms
+def printer (speakers)
+  batch_badge_creator(speakers).each do |guests|
+    puts guests
+  end
+  
+  assign_rooms(speakers).each do |room_number|
+    puts room_number
+  end
 end
