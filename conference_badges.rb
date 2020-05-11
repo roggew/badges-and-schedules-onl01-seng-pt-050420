55a0a7a7c66b93201750c04fed46c2fea0bfb3bf
William Rogge <williamroggeiv@gmail.com>
2020-05-11 03:28:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  message = []
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  message <<  attendees.each {|attendees| puts "Hello, my name is #{attendees}."}
+end
