4df6a0a1f30d3638e2b03b327981cadf05dbcf55
William Rogge <williamroggeiv@gmail.com>
2020-05-11 04:11:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,6 +12,6 @@ def batch_badge_creator(speakers)
 end
 
 def assign_rooms(speakers)
-  room_number[]
+  room_number = []
   speakers.each_with_index {|name, index| room_number << "Hello, #{name}! You'll be assined to room #{index+1}!"
 end
